3654d997f8815a5b88969f3427a824c0ac0f6fc7
Garen Lam <117001562+lphucdo@users.noreply.github.com>
2024-09-14 13:41:53 +0700

    V0.0.1
    Khoi Tao project

---
+++ LiveDashboard-Kafka/src/main/java/com/lamdo/LiveDashboardKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.lamdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveDashboardKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveDashboardKafkaApplication.class, args);
+	}
+
+}
